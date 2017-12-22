3e43c5530e1511f2265f5748dc5763eb03c427f6
t-kusakabe <ttmac0704@gmail.com>
2017-12-22 22:29:34 +0900

    init

---
+++ lib/gem_sample.rb
@@ -0,0 +1,7 @@
+require "gem_sample/version"
+
+module GemSample extend self
+  def hello
+    'Hello OIC ITCreate Club 第一 Advent Calendar 2017'
+  end
+end
---
+++ lib/gem_sample/version.rb
@@ -0,0 +1,3 @@
+module GemSample
+  VERSION = "0.1.0"
+end
